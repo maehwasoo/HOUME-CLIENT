313b98ee4608f2cc9c6b68e76b06c7baf6971668
maehwasoo <kheom0512@gmail.com>
2026-01-09 11:18:11 +0000

    chore: Firebase Analytics 활성화 플래그 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_ENABLE_FIREBASE_ANALYTICS?: string;
+}
+
 declare module '*.svg?react' {
   import * as React from 'react';
   const ReactComponent: React.FunctionComponent<
